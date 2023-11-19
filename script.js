b89a0cf06309b694f1f574d73b940bf7ee944e95
Pravin Patil <106026547+pravin8007@users.noreply.github.com>
2023-11-19 10:52:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+  function getBrowserInfo() {
+        // Using navigator object to retrieve browser information
+        var browserName = navigator.appName;
+        var browserVersion = navigator.appVersion;
+
+        // Displaying the information in the specified format
+        var browserInfoMessage = "You are using " + browserName + " version " + browserVersion;
+        
+        // Adding the message to the div with id 'browser-info'
+        document.getElementById('browser-info').innerText = browserInfoMessage;
+    }
+
+    // Calling the function on page load
+    window.onload = getBrowserInfo;
